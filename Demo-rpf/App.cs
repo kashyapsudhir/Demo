8fdd7f4bb29ea7570c28bb1f671f8c154a48945b
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-04-20 14:15:09 +0530

    [sudhir]Add: DEMO App

---
+++ Demo-rpf/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_rpf
+{
+    public class App
+    {
+        public void Add()
+        {
+            int i = 1 + 2;
+            Console.WriteLine("Hello hii",i);
+            Console.WriteLine("Sudhir");
+        }
+    }
+}
